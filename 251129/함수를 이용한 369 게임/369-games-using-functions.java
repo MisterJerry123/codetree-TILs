5090427f4e60acdbcf54f28fb9c8770322f6ee0e
KunHee Jung <misterjerry12345@gmail.com>
2025-11-29 19:35:20 +0900

    Title: 함수를 이용한 369 게임, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 251129/함수를 이용한 369 게임/369-games-using-functions.java
+++ 251129/함수를 이용한 369 게임/369-games-using-functions.java
@@ -4,9 +4,9 @@ public class Main{
         int cnt=0;
         for(int i= start;i<=end;i++){
             int init=i;
-            while(init>0){
-                int t = i%10;
-                if(t==3 || t==6 || t==9 || init%3==0){
+        while(init!=0){
+                int t = init%10;
+                if(t==3 || t==6 || t==9 || i%3==0){
                     cnt++;
                     //System.out.println("통과"+ init);
 
